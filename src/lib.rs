ca2eac04bc86833c16170cb95ea74127ebc15b47
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-08-09 18:45:07 -0400

    Add missing traits

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,15 @@
 #![warn(missing_docs)]
 #![warn(unused)]
 
-use std::borrow::Cow;
-use std::ffi::OsString;
+use std::borrow::{Borrow, Cow};
+use std::cmp::Ordering;
+use std::convert::Infallible;
+use std::ffi::{OsStr, OsString};
+use std::hash::{Hash, Hasher};
 use std::ops::{Deref, DerefMut, Div};
-use std::path::{Path, PathBuf};
+use std::path::{Iter, Path, PathBuf};
 use std::rc::Rc;
+use std::str::FromStr;
 use std::sync::Arc;
 
 /// A PathBuf wrapper that has support for a Path DSL.
@@ -55,6 +59,12 @@ impl AsRef<Path> for PathDSL {
 impl AsMut<PathBuf> for PathDSL {
     fn as_mut(&mut self) -> &mut PathBuf {
         &mut self.path
+    }
+}
+
+impl AsRef<OsStr> for PathDSL {
+    fn as_ref(&self) -> &OsStr {
+        self.path.as_ref()
     }
 }
 
@@ -154,6 +164,12 @@ impl<'a> Into<Cow<'a, Path>> for &'a PathDSL {
     }
 }
 
+impl<'a> Into<Cow<'a, OsStr>> for &'a PathDSL {
+    fn into(self) -> Cow<'a, OsStr> {
+        Cow::Borrowed(self.path.as_os_str())
+    }
+}
+
 impl<'a> Into<Arc<Path>> for PathDSL {
     fn into(self) -> Arc<Path> {
         self.path.into()
@@ -163,6 +179,172 @@ impl<'a> Into<Arc<Path>> for PathDSL {
 impl<'a> Into<Rc<Path>> for PathDSL {
     fn into(self) -> Rc<Path> {
         self.path.into()
+    }
+}
+
+////////////////
+// Partial Eq //
+////////////////
+
+impl PartialEq<PathDSL> for PathDSL {
+    fn eq(&self, other: &PathDSL) -> bool {
+        self.path == other.path
+    }
+}
+
+impl PartialEq<PathBuf> for PathDSL {
+    fn eq(&self, other: &PathBuf) -> bool {
+        self.path == *other
+    }
+}
+
+impl PartialEq<Path> for PathDSL {
+    fn eq(&self, other: &Path) -> bool {
+        self.path.as_path() == other
+    }
+}
+
+impl PartialEq<OsStr> for PathDSL {
+    fn eq(&self, other: &OsStr) -> bool {
+        self.path.as_path() == other
+    }
+}
+
+impl PartialEq<OsString> for PathDSL {
+    fn eq(&self, other: &OsString) -> bool {
+        self.path.as_path() == other
+    }
+}
+
+impl<'a> PartialEq<Cow<'a, Path>> for PathDSL {
+    fn eq(&self, other: &Cow<'a, Path>) -> bool {
+        self.path.as_path() == other
+    }
+}
+
+impl<'a> PartialEq<Cow<'a, OsStr>> for PathDSL {
+    fn eq(&self, other: &Cow<'a, OsStr>) -> bool {
+        self.path.as_path() == other
+    }
+}
+
+////////
+// Eq //
+////////
+
+impl Eq for PathDSL {}
+
+/////////////////
+// Partial Ord //
+/////////////////
+
+impl PartialOrd<PathDSL> for PathDSL {
+    fn partial_cmp(&self, other: &PathDSL) -> Option<Ordering> {
+        self.path.partial_cmp(&other.path)
+    }
+}
+
+impl PartialOrd<PathBuf> for PathDSL {
+    fn partial_cmp(&self, other: &PathBuf) -> Option<Ordering> {
+        self.path.partial_cmp(other)
+    }
+}
+
+impl PartialOrd<Path> for PathDSL {
+    fn partial_cmp(&self, other: &Path) -> Option<Ordering> {
+        self.path.as_path().partial_cmp(other)
+    }
+}
+
+impl<'a> PartialOrd<Cow<'a, Path>> for PathDSL {
+    fn partial_cmp(&self, other: &Cow<'a, Path>) -> Option<Ordering> {
+        self.path.as_path().partial_cmp(other)
+    }
+}
+
+impl<'a> PartialOrd<Cow<'a, OsStr>> for PathDSL {
+    fn partial_cmp(&self, other: &Cow<'a, OsStr>) -> Option<Ordering> {
+        self.path.as_path().partial_cmp(other.into())
+    }
+}
+
+impl PartialOrd<OsStr> for PathDSL {
+    fn partial_cmp(&self, other: &OsStr) -> Option<Ordering> {
+        self.path.as_path().partial_cmp(other.into())
+    }
+}
+
+impl PartialOrd<OsString> for PathDSL {
+    fn partial_cmp(&self, other: &OsString) -> Option<Ordering> {
+        self.path.as_path().partial_cmp(other.into())
+    }
+}
+
+/////////
+// Ord //
+/////////
+
+impl Ord for PathDSL {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.path.cmp(&other.path)
+    }
+}
+
+/////////////
+// FromStr //
+/////////////
+
+impl FromStr for PathDSL {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        PathBuf::from_str(s).map(|path| PathDSL { path })
+    }
+}
+
+//////////
+// Hash //
+//////////
+
+impl Hash for PathDSL {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.path.hash(state)
+    }
+}
+
+////////////
+// Extend //
+////////////
+
+impl<P> Extend<P> for PathDSL
+where
+    P: AsRef<Path>,
+{
+    fn extend<I: IntoIterator<Item = P>>(&mut self, iter: I) {
+        self.path.extend(iter)
+    }
+}
+
+//////////////////
+// FromIterator //
+//////////////////
+
+impl<'a> IntoIterator for &'a PathDSL {
+    type Item = &'a OsStr;
+    type IntoIter = Iter<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.path.as_path().into_iter()
+    }
+}
+
+/////////////
+// Default //
+/////////////
+
+impl Borrow<Path> for PathDSL {
+    fn borrow(&self) -> &Path {
+        self.path.borrow()
     }
 }
 
@@ -252,6 +434,15 @@ impl Div<Cow<'_, Path>> for PathDSL {
     }
 }
 
+impl Div<Cow<'_, OsStr>> for PathDSL {
+    type Output = PathDSL;
+
+    fn div(mut self, rhs: Cow<'_, OsStr>) -> Self::Output {
+        self.path.push(rhs);
+        self
+    }
+}
+
 ///////////
 // Div & //
 ///////////
@@ -323,6 +514,16 @@ impl Div<Cow<'_, Path>> for &PathDSL {
     type Output = PathDSL;
 
     fn div(self, rhs: Cow<'_, Path>) -> Self::Output {
+        let mut new_self = (*self).clone();
+        new_self.path.push(rhs);
+        new_self
+    }
+}
+
+impl Div<Cow<'_, OsStr>> for &PathDSL {
+    type Output = PathDSL;
+
+    fn div(self, rhs: Cow<'_, OsStr>) -> Self::Output {
         let mut new_self = (*self).clone();
         new_self.path.push(rhs);
         new_self
