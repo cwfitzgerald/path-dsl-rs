6800639b762e52c04ad0490d1cfd3f301c42065b
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-08-15 13:34:21 -0400

    Use more intuitive names in macros

--- tests/unit-tests.rs
+++ tests/unit-tests.rs
@@ -4,15 +4,15 @@ use std::ffi::{OsStr, OsString};
 use std::path::{Path, PathBuf};
 
 macro_rules! dsl_test {
-    ($(first: $first:path,)? $(second: ($($conv:tt)+),)? $(self: ($($selfmod:tt)+),)? name: $id:ident) => {
+    ($(constructor: $constructor:path,)? $(converter: ($($conv:tt)+),)? $(self: ($($selfmod:tt)+),)? name: $id:ident) => {
         #[allow(unused)]
         #[test]
         fn $id() {
-            let mut first = $($first)?("ident");
+            let mut first = $($constructor)?("ident");
             let second = $($($conv)+)?(first);
             let res_dsl = $($($selfmod)+)?PathDSL::new() / second / "my_file";
 
-            let mut first = $($first)?("ident");
+            let mut first = $($constructor)?("ident");
             let second = $($($conv)+)?(first);
             let res_macro = path!(second | "my_file");
 
@@ -27,31 +27,31 @@ macro_rules! dsl_test {
 }
 
 macro_rules! owned_dsl_test {
-    ($(first: $first:path,)? name: $name:ident) => {
-        paste::item!(dsl_test!{$(first: $first,)? name: [<dsl_ $name>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&), name: [<dsl_ $name _ref>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&mut), name: [<dsl_ $name _ref_mut>]});
+    ($(constructor: $constructor:path,)? name: $name:ident) => {
+        paste::item!(dsl_test!{$(constructor: $constructor,)? name: [<dsl_ $name>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&), name: [<dsl_ $name _ref>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&mut), name: [<dsl_ $name _ref_mut>]});
 
-        paste::item!(dsl_test!{$(first: $first,)? self: (&), name: [<dsl_ref_ $name>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&), self: (&), name: [<dsl_ref_ $name _ref>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&mut), self: (&), name: [<dsl_ref_ $name _ref_mut>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? self: (&), name: [<dsl_ref_ $name>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&), self: (&), name: [<dsl_ref_ $name _ref>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&mut), self: (&), name: [<dsl_ref_ $name _ref_mut>]});
 
-        paste::item!(dsl_test!{$(first: $first,)? self: (&mut), name: [<dsl_ref_mut_ $name>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&), self: (&mut), name: [<dsl_ref_mut_ $name _ref>]});
-        paste::item!(dsl_test!{$(first: $first,)? second: (&mut), self: (&mut), name: [<dsl_ref_mut_ $name _ref_mut>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? self: (&mut), name: [<dsl_ref_mut_ $name>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&), self: (&mut), name: [<dsl_ref_mut_ $name _ref>]});
+        paste::item!(dsl_test!{$(constructor: $constructor,)? converter: (&mut), self: (&mut), name: [<dsl_ref_mut_ $name _ref_mut>]});
     };
 }
 
-owned_dsl_test!(first: OsStr::new, name: osstr);
-owned_dsl_test!(first: OsString::from, name: osstring);
+owned_dsl_test!(constructor: OsStr::new, name: osstr);
+owned_dsl_test!(constructor: OsString::from, name: osstring);
 owned_dsl_test!(name: str);
-owned_dsl_test!(first: String::from, name: string);
-owned_dsl_test!(first: Path::new, name: path);
-owned_dsl_test!(first: PathBuf::from, name: pathbuf);
-owned_dsl_test!(first: PathDSL::from, name: dsl);
-owned_dsl_test!(first: gen_box_path, name: box_path);
-owned_dsl_test!(first: gen_cow_path, name: cow_path);
-owned_dsl_test!(first: gen_cow_osstr, name: arc_osstr);
+owned_dsl_test!(constructor: String::from, name: string);
+owned_dsl_test!(constructor: Path::new, name: path);
+owned_dsl_test!(constructor: PathBuf::from, name: pathbuf);
+owned_dsl_test!(constructor: PathDSL::from, name: dsl);
+owned_dsl_test!(constructor: gen_box_path, name: box_path);
+owned_dsl_test!(constructor: gen_cow_path, name: cow_path);
+owned_dsl_test!(constructor: gen_cow_osstr, name: arc_osstr);
 
 fn gen_box_path(p: &str) -> Box<Path> {
     Box::from(Path::new(p))
